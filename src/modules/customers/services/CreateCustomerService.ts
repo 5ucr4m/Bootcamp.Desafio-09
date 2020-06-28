7814304fbc13c6db1c015addfd718ccb903d8604
Marcus Vinicius <5ucr4m@gmail.com>
2020-06-28 16:50:55 -0300

    Init

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,28 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const findedCustomer = await this.customersRepository.findByEmail(email);
+
+    if (findedCustomer) {
+      throw new AppError('Email já está em uso');
+    }
+
+    try {
+      const customer = this.customersRepository.create({
+        name,
+        email,
+      });
+      return customer;
+    } catch (err) {
+      console.log(err);
+      throw new AppError('Não foi possivel criar o customer');
+    }
   }
 }
 
